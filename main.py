343bdc0e1ce29d70037b6cf33e011bab8d49e6bc
bigboysneaky <nikonpun99@gmail.com>
2026-01-17 16:13:01 +1100

    Fourth commit I think. Just pushing cuz thing're working lol

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +20,9 @@ def main():
                 return 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
         
 
 if __name__ == "__main__":
